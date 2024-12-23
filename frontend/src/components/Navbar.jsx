ce187f60868456c2e205623ade30bf90b7570fa7
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-23 20:14:31 +0500

    Updated

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <p>My Fitness center </p>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkoutSessions = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WorkoutSessions
